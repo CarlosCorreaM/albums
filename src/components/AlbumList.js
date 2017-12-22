0ee56298e712b5d87eeaf73703a0e017577255bc
Carlos Correa <carloscorrea.dev@gmail.com>
2017-12-22 00:24:42 -0600

    class based component and fetching data

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Album list</Text>
-        </View>
-    );
-};
+class AlbumList extends Component {
 
+    state = {
+        albums: []
+    };
+
+    componentDidMount(){
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({
+                albums: response.data
+            }));
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Album list</Text>
+            </View>
+        );
+    }
+}
 export default AlbumList;
