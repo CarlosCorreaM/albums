077bbf9dfcf49ae6cab166608857c3738c9cb702
Carlos Correa <carloscorrea.dev@gmail.com>
2017-12-26 17:46:48 -0600

    card and cardsection components ready

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView  } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -14,19 +14,17 @@ class AlbumList extends Component {
             .then(response => this.setState({
                 albums: response.data
             }));
-    }
+    };
 
     renderAlbums(){
        return this.state.albums.map(album =>
-       <AlbumDetail key={ album.title } album={album} />
-    );
-    }
+       <AlbumDetail key={ album.title } album={album} />)}
 
     render() {
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = (props) => {
+const Card = ( props ) => {
     return (
         <View style={ styles.containerStyle }>
-            {props.children}
+            { props.children }
+            {/* props.children will always put inner 
+            content of component inside the children component*/}
         </View>
     );
 };
